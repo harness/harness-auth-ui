79e11f8ca607b3463121e4646c875bd4d7e02f88
nanaxuHarness <80717697+nanaxuHarness@users.noreply.github.com>
2021-05-03 16:31:11 -0500

    feat: [GTM-152] add signup telemetry tracking

---
+++ src/telemetry/Telemetry.ts
@@ -0,0 +1,7 @@
+import Telemetry from "@wings-software/telemetry";
+
+const TelemetryInstance = new Telemetry(
+  window.segmentToken || "exa6lo7CnJXqKnR83itMpHYLY5fiajft"
+);
+
+export default TelemetryInstance;
