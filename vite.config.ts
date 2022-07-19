633c47fe7aea54a94b7c3410f8ddd3caf55282e7
avinashmadhwani02 <62028553+avinashmadhwani02@users.noreply.github.com>
2022-07-19 20:11:29 +0530

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -82,6 +82,7 @@ export default defineConfig({
           BugsnagSourceMapUploaderPlugin({
             apiKey: BUGSNAG_TOKEN,
             appVersion: version,
+            base: "/assets",
             overwrite: true
           })
         ]
